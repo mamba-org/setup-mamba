574b5e7d168f036eaf88a647f5c397d19dfe1afa
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-06-05 15:24:01 +0200

    Add command to install mamba (#3)
    * Add command to install mamba
    
    * Remove wait from the tests
    
    * Add test step to install Python with mamba
    
    * Update dist/index.js
    
    * Add to path
    
    * Add more steps to the test workflow
    
    * Test on mac os
    
    * Use condabin on mac os
    
    * Debug path
    
    * Switch order
    
    * Fix permissions
    
    * Add windows
    
    * Debug windows path
    
    * Update path on windows
    
    * Try libssh2=1.8.2
    
    * Revert "Try libssh2=1.8.2"
    
    This reverts commit 44bc7a4a964068902ef935e4da198fd67219eded.
    
    * Update README and package.json
    
    * Update action.yml
    
    * Remove windows for now

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,51 @@
+import * as path from 'path'
+
 import * as core from '@actions/core'
-import {wait} from './wait'
+
+import * as exec from '@actions/exec'
+
+async function fixPermissions(os: string): Promise<void> {
+  if (os === 'darwin') {
+    const username = process.env.USER
+    await exec.exec('sudo', [
+      'chown',
+      '-R',
+      `${username}:staff`,
+      process.env.CONDA as string
+    ])
+  }
+}
+
+async function addPath(os: string): Promise<void> {
+  const basePath = process.env.CONDA as string
+  if (os === 'darwin') {
+    const bin = path.join(basePath, 'condabin')
+    core.addPath(bin)
+  } else if (os === 'win32') {
+    const bin = path.join(basePath, 'Scripts')
+    core.addPath(bin)
+  } else {
+    const bin = path.join(basePath, 'bin')
+    core.addPath(bin)
+  }
+}
+
+async function installMamba(): Promise<void> {
+  await exec.exec('conda', ['install', '-y', '-c', 'conda-forge', 'mamba'])
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const os = process.platform
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug('Fix permissions')
+    fixPermissions(os)
 
-    core.setOutput('time', new Date().toTimeString())
+    core.debug('Add conda to the path')
+    await addPath(os)
+
+    core.debug('Installing mamba')
+    await installMamba()
   } catch (error) {
     core.setFailed(error.message)
   }
